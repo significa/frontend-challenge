d462e53b80c407a4ed9ab5ed122e51995c0d3bc1
Henrique Melanda <hjdesigner23@gmail.com>
2018-10-17 11:14:45 -0300

    Initial commit with project basis

---
+++ my-code/src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React, { PureComponent } from 'react'
+
+class App extends PureComponent {
+  render () {
+    return (
+      <div>
+        <h1>Base Project</h1>
+      </div>
+    )
+  }
+}
+
+export default App
