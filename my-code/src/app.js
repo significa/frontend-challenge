ef348bf3a4207bfadae9460774b4cc41065dbe0f
Henrique Melanda <hjdesigner23@gmail.com>
2018-10-22 08:10:25 -0300

    I started developing the front of the movie showcase

--- my-code/src/app.js
+++ my-code/src/app.js
@@ -3,6 +3,7 @@
 import React, { PureComponent } from 'react'
 import Header from 'components/header'
 import Search from 'components/search'
+import Showcase from 'components/showcase'
 import './app.scss'
 
 class App extends PureComponent {
@@ -11,6 +12,7 @@ class App extends PureComponent {
       <div className='container'>
         <Header />
         <Search />
+        <Showcase />
       </div>
     )
   }
---
+++ my-code/src/components/showcase/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import './showcase.scss'
+
+const Showcase = () => (
+  <ul className='showcase'>
+    <li className='showcase-item'>
+      <a href='#'>
+        <div className='showcase-item__mask' />
+        <figure className='showcase-item__image'>
+          <img src='https://static.posters.cz/image/750/posters/sons-of-anarchy-jax-skull-i22296.jpg' alt='Sons Of Arnarchy' />
+        </figure>
+        <h2 className='showcase-item__title'>Sons Of Anarchy</h2>
+        <small className='showcase-item__date'>2018</small>
+      </a>
+    </li>
+    <li className='showcase-item'>
+      <a href='#'>
+        <div className='showcase-item__mask' />
+        <figure className='showcase-item__image'>
+          <img src='https://kanto.legiaodosherois.com.br/fnoop/wp-content/uploads/2017/05/legiao_p63tJcfZOK2wCuPFmRgHU_LBibIqjVaWNe5D94oMSy.jpg' alt='Spider-Man' />
+        </figure>
+        <h2 className='showcase-item__title'>Spider-Man</h2>
+        <small className='showcase-item__date'>2017</small>
+      </a>
+    </li>
+    <li className='showcase-item'>
+      <a href='#'>
+        <div className='showcase-item__mask' />
+        <figure className='showcase-item__image'>
+          <img src='http://www.bandodequadrados.com/img/imagem_noticia/a15820a3703c656b2330813e2eb228ba.jpg' alt='Ready Player One' />
+        </figure>
+        <h2 className='showcase-item__title'>Ready Player One</h2>
+        <small className='showcase-item__date'>2018</small>
+      </a>
+    </li>
+    <li className='showcase-item'>
+      <a href='#'>
+        <div className='showcase-item__mask' />
+        <figure className='showcase-item__image'>
+          <img src='https://static.posters.cz/image/750/posters/sons-of-anarchy-jax-skull-i22296.jpg' alt='Sons Of Arnarchy' />
+        </figure>
+        <h2 className='showcase-item__title'>Sons Of Anarchy</h2>
+        <small className='showcase-item__date'>2018</small>
+      </a>
+    </li>
+    <li className='showcase-item'>
+      <a href='#'>
+        <div className='showcase-item__mask' />
+        <figure className='showcase-item__image'>
+          <img src='https://kanto.legiaodosherois.com.br/fnoop/wp-content/uploads/2017/05/legiao_p63tJcfZOK2wCuPFmRgHU_LBibIqjVaWNe5D94oMSy.jpg' alt='Spider-Man' />
+        </figure>
+        <h2 className='showcase-item__title'>Spider-Man</h2>
+        <small className='showcase-item__date'>2017</small>
+      </a>
+    </li>
+    <li className='showcase-item'>
+      <a href='#'>
+        <div className='showcase-item__mask' />
+        <figure className='showcase-item__image'>
+          <img src='http://www.bandodequadrados.com/img/imagem_noticia/a15820a3703c656b2330813e2eb228ba.jpg' alt='Ready Player One' />
+        </figure>
+        <h2 className='showcase-item__title'>Ready Player One</h2>
+        <small className='showcase-item__date'>2018</small>
+      </a>
+    </li>
+  </ul>
+)
+
+export default Showcase
