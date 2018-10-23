02f57cdf6c581fe51be352cfda9eaa813ba2e56c
Isabel Sá <25415758+isabelsoasa@users.noreply.github.com>
2018-10-23 11:49:15 +0100

    Initial setup for devDependencies

---
+++ my-code/src/App.js
@@ -0,0 +1,16 @@
+// @flow
+import React from "react"
+
+type Props = {}
+
+class App extends React.Component<Props> {
+  render() {
+    return (
+      <div>
+        <p>Initial setup.</p>
+      </div>
+    )
+  }
+}
+
+export default App
