f436dae000517faae05a12a4641680e41a27927b
Andrea Toso <andrea.toso99@gmail.com>
2018-10-27 14:50:29 +0200

    Add eslint parser

--- my-code/frontend-challenge-react/.eslintrc.js
+++ my-code/frontend-challenge-react/.eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
     "extends": "airbnb",
+    "parser": "babel-eslint",
+
     "rules": {
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     },
