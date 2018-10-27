a3218e802c2589e1f94f92d620a77cc5924ac608
Andrea Toso <andrea.toso99@gmail.com>
2018-10-27 09:18:01 +0200

    Convert index css to scss

--- my-code/frontend-challenge-react/src/index.js
+++ my-code/frontend-challenge-react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import Search from './pages/search/Search';
 import * as serviceWorker from './serviceWorker';
 
