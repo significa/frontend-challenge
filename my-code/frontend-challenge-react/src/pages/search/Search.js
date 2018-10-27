b6035425ebb9271933921832460209db93fe6ca5
Andrea Toso <andrea.toso99@gmail.com>
2018-10-27 10:28:06 +0200

    Complete initial search page with illustration image

--- my-code/frontend-challenge-react/src/pages/search/Search.js
+++ my-code/frontend-challenge-react/src/pages/search/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Search.module.scss';
 import Header from '../../components/header/Header';
 import SearchBar from '../../components/searchBar/SearchBar';
+import searchIllustration from '../../img/searchIllustration.png';
 
 const Search = () => (
   <div className={styles.container}>
@@ -10,6 +11,19 @@ const Search = () => (
         <div className="box">
           <Header />
           <SearchBar />
+
+          <div className={styles.emptyContainer}>
+            <img src={searchIllustration} alt="illustration" className={styles.searchIllustration} />
+            <div>
+              <h3 className={styles.searchIllustrationDesc}>
+                Don't know what to
+                <br />
+                search?
+              </h3>
+
+            </div>
+            <p className={styles.searchIllustrationSecondDesc}>Here's an offer you can't refuse</p>
+          </div>
         </div>
       </div>
     </div>
