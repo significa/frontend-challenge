812c3c88baebd6a9a0b798461b7ba0f1bce22062
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-26 05:01:03 -0200

    :rotating_light:

--- my-code/src/App.js
+++ my-code/src/App.js
@@ -1,17 +1,16 @@
 import React, { Fragment } from 'react'
 import Navbar from 'components/Navbar'
-import Container from 'components/Container'
-import { Row, Cell } from 'components/Grid'
 import Searchbar from 'components/Searchbar'
-import Text from 'components/Text'
 import EmptyState from 'components/EmptyState'
 
-const App = () => (
-	<Fragment>
-		<Navbar/>
-		<Searchbar/>
-		<EmptyState/>
-	</Fragment>
-)
+const App = () => {
+	return(
+		<Fragment>
+			<Navbar/>
+			<Searchbar/>
+			<EmptyState/>
+		</Fragment>
+	)
+}
 
 export default App
