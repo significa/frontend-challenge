758bdedde3dc738cd85d9d3e200a80d7960e32d3
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-26 02:32:44 -0200

    :sparkles: add theme colors

--- my-code/src/index.js
+++ my-code/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+import theme from './theme'
 import App from './App'
 import { register } from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Wrapper = () => (
+	<ThemeProvider theme={theme}>
+		<App/>
+	</ThemeProvider>
+)
+
+ReactDOM.render(<Wrapper />, document.getElementById('root'))
 
 register()
---
+++ my-code/src/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+	colors: {
+		// main colors
+		yellow:    '#FF9F1C',
+		red:       '#FF4040',
+		green:     '#2EC4B',
+		// grey colors
+		dark:      '#0A1014',
+		grey:      '#1B2329',
+		midGrey:   '#353F4C',
+		lightGrey: '#7A8C99',
+		white:     '#FFFFFF',
+	},
+}
+
+export default theme
