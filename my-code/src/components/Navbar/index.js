5f044fb093c6749985763c178a0796cfd5d0c949
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-26 18:14:25 -0200

    :sparkles: Navbar logo navigation

--- my-code/src/components/Navbar/index.js
+++ my-code/src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useUrlState } from 'with-url-state'
 import Container from 'components/Container'
 import { Logo } from 'components/Icon'
 
@@ -7,12 +8,15 @@ const Wrapper = styled.div`
 	padding: 1.5rem 0;
 `
 
-const Navbar = () => (
-	<Wrapper>
-		<Container>
-			<Logo/>
-		</Container>
-	</Wrapper>
-)
+const Navbar = () => {
+	const [, setUrlState] = useUrlState()
+	return(
+		<Wrapper>
+			<Container>
+				<Logo onClick={() => setUrlState({index:1})}/>
+			</Container>
+		</Wrapper>
+	)
+}
 
 export default Navbar
