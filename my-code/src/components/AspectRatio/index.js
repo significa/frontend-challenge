cbe4b27b2b7fe4dd6a14b8dcfd7814c8892fc987
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-26 08:26:17 -0200

    :sparkles: add AspectRatio

---
+++ my-code/src/components/AspectRatio/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Outer = styled.div`
+	height: 0;
+	overflow: hidden;
+	padding-top: ${p => `${1/p.ratio * 100}%`};
+	position: relative;
+`
+
+const Inner = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+`
+
+const AspectRatio = ({ratio = 1, children, props}) => (
+	<Outer ratio={ratio} {...props}>
+		<Inner>{children}</Inner>
+	</Outer>
+)
+
+export default AspectRatio
