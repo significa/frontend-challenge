4574d1ce1b631adfeb215b534e8d322d5e9bd20a
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-26 08:26:03 -0200

    :sparkles: add set + uniqueArray hooks

--- my-code/src/utils/hooks.js
+++ my-code/src/utils/hooks.js
@@ -4,3 +4,16 @@ export const useInputState = initialState => {
 	const [state, setState] = useState(initialState)
 	return [state, e => setState(e.target.value)]
 }
+
+export const useSet = (initialValue = []) => {
+	const [value, setValue] = useState(new Set(initialValue))
+	const add = item => !!item && setValue(value.add(item))
+	const remove = item => value.delete(item) && setValue(value)
+	const toggle = item => value.has(item) ? remove(item) : add(item)
+	return [value, {add, remove, toggle}]
+}
+
+export const useUniqueArray = (initialValue = []) => {
+	const [value, actions] = useSet(initialValue)
+	return [[...value], actions]
+}
