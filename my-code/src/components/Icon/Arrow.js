79274e850509077ba911e0b2945f9f578328b3c6
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-26 14:34:28 -0200

    :bento: add more icons

---
+++ my-code/src/components/Icon/Arrow.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Heart = ({
+	size = 24,
+	filled = false,
+	color = 'currentColor',
+	style,
+	...props
+}) => (
+	<svg
+		viewBox='0 0 24 24'
+		strokeLinecap='round'
+		strokeLinejoin='round'
+		width={size}
+		height={size}
+		style={{display: 'block', ...style}}
+		xmlns='http://www.w3.org/2000/svg'
+		{...props}
+	>
+		<g stroke={color} stroke-width='2' fill='none'>
+			<path d='M9 19.07L1.93 12 9 4.93'/>
+			<path d='M3 12h20'/>
+		</g>
+	</svg>
+)
+
+export default Heart
--- my-code/src/components/Icon/index.js
+++ my-code/src/components/Icon/index.js
@@ -1,7 +1,10 @@
 import Logo from './Logo'
 import Heart from './Heart'
+import Arrow from './Arrow'
+import IMDB from './IMDB'
+import RottenTomatoes from './RottenTomatoes'
 import Movie from './Movie'
 import Dead from './Dead'
 import FightClub from './FightClub'
 
-export { Logo, Heart, Movie, Dead, FightClub }
+export { Logo, Heart, Arrow, IMDB, RottenTomatoes, Movie, Dead, FightClub }
