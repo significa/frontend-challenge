36a4143a530bda3937ef4cc2700b563a9cfb82dd
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-26 02:45:29 -0200

    :wrench: set NODE_PATH

--- my-code/src/App.js
+++ my-code/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Container from './components/Container'
+import Container from 'components/Container'
 
 const App = () => <Container>app</Container>
 
--- my-code/src/index.js
+++ my-code/src/index.js
@@ -2,10 +2,10 @@ import React, {Fragment} from 'react'
 import ReactDOM from 'react-dom'
 import 'typeface-roboto'
 import { ThemeProvider } from 'styled-components'
-import GlobalStyle from './components/GlobalStyle'
+import GlobalStyle from 'components/GlobalStyle'
 import theme from './theme'
 import App from './App'
-import { register } from './serviceWorker'
+import { register } from 'serviceWorker'
 
 const Wrapper = () => (
 	<ThemeProvider theme={theme}>
