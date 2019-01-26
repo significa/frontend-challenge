76bd00b295b50522d1c185557b2772f8efb5609a
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-26 08:26:37 -0200

    :sparkles: add Heart

---
+++ my-code/src/components/Heart/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Heart = ({
+	size = 24,
+	filled = false,
+	color = 'currentColor',
+	style,
+	...props
+}) => (
+	<svg
+		viewBox="0 0 24 24"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		width={size}
+		height={size}
+		style={{display: 'block', ...style}}
+		xmlns='http://www.w3.org/2000/svg'
+		{...props}
+	>
+		<path
+			strokeWidth='2'
+			stroke={color}
+			fill={filled ? color : 'none'}
+			d='M12 5.74C24.32-3.88 26.31 14.49 12 20-2.31 15.57-.32-3.88 12 5.74z'
+		/>
+	</svg>
+)
+
+export default Heart
