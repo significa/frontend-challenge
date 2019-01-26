1db1d1139fbda6fb843d6215a7bbf74bde4e9d32
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-26 05:26:06 -0200

    :sparkles: useFetch to get movie data

--- my-code/src/App.js
+++ my-code/src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { useFetch } from 'react-hooks-fetch'
 import { useInputState } from 'utils/hooks'
 import Navbar from 'components/Navbar'
 import Searchbar from 'components/Searchbar'
@@ -6,10 +7,18 @@ import EmptyState from 'components/EmptyState'
 
 const App = () => {
 	const [search, setSearch] = useInputState('')
+
+	const { error, loading, data } = useFetch([
+		`https://api.themoviedb.org/3/search/movie`,
+		`?api_key=${process.env.REACT_APP_TMDB_KEY}`,
+		`&query=${search}`,
+	].join(''))
+
 	return(
 		<Fragment>
 			<Navbar/>
 			<Searchbar value={search} onChange={setSearch}/>
+			{search && <pre>{JSON.stringify({error, loading, data}, null , 2)}</pre>}
 			{!search && <EmptyState/>}
 		</Fragment>
 	)
