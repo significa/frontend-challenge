efef2b40abc3db1646d6b12806eec713ddfe5a11
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-28 02:32:41 -0200

    :art: babel export Icon

--- my-code/src/components/Icon/index.js
+++ my-code/src/components/Icon/index.js
@@ -1,10 +1,8 @@
-import Logo from './Logo'
-import Heart from './Heart'
-import Arrow from './Arrow'
-import IMDB from './IMDB'
-import RottenTomatoes from './RottenTomatoes'
-import Movie from './Movie'
-import Dead from './Dead'
-import FightClub from './FightClub'
-
-export { Logo, Heart, Arrow, IMDB, RottenTomatoes, Movie, Dead, FightClub }
+export Logo from './Logo'
+export Heart from './Heart'
+export Arrow from './Arrow'
+export IMDB from './IMDB'
+export RottenTomatoes from './RottenTomatoes'
+export Movie from './Movie'
+export Dead from './Dead'
+export FightClub from './FightClub'
