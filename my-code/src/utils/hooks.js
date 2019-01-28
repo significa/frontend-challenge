e53096baad7d8a4b010e52e574a8cdcc4f497d1c
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-28 04:22:25 -0200

    :fire: remove unused stuff

--- my-code/src/utils/hooks.js
+++ my-code/src/utils/hooks.js
@@ -1,24 +1,6 @@
 import { useState } from 'react'
-import { useUrlState as useUrl } from 'with-url-state'
 
 export const useInputState = initialState => {
 	const [state, setState] = useState(initialState)
 	return [state, e => setState(e.target.value)]
 }
-
-export const useSet = (initialValue = []) => {
-	const [value, setValue] = useState(new Set(initialValue))
-	const add = item => !!item && setValue(value.add(item))
-	const remove = item => value.delete(item) && setValue(value)
-	const toggle = item => value.has(item) ? remove(item) : add(item)
-	return [value, {add, remove, toggle}]
-}
-
-export const useUniqueArray = (initialValue = []) => {
-	const [value, actions] = useSet(initialValue)
-	return [[...value], actions]
-}
-
-export const useUrlState = (initialState = {}) => (
-	useUrl(initialState, {shouldPushState: () => true})
-)
