0ab04ea1d1e3ebbf29472fe47dcad5a74f8d3073
João Marques <joao@jjmf.com>
2020-01-06 22:52:47 -0300

    PropTypes for LoadingIndicator. Updated Readme.

--- my-code/src/components/LoadingIndicator/index.js
+++ my-code/src/components/LoadingIndicator/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Container } from './styles';
 
 export default function LoadingIndicator({ text }) {
   return <Container>{text}</Container>;
 }
+
+LoadingIndicator.propTypes = {
+  text: PropTypes.string.isRequired,
+};
