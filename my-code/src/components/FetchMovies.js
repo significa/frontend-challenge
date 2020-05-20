51ab9a8cd49c5026f726f5b0c7a3231faed91dcf
costa86 <costa86@zoho.com>
2020-05-20 19:57:35 +0100

    fix on movie not found

--- my-code/src/components/FetchMovies.js
+++ my-code/src/components/FetchMovies.js
@@ -3,11 +3,19 @@ import { Movie } from "./Movie";
 
 export function FetchMovies(props) {
     const [data, setData] = useState([]);
+    const [found, setFound] = useState(false);
+
     async function getData() {
         try {
             let req = await fetch(props.url);
             let json = await req.json();
-            setData(json.Search);
+            if (json.Search) {
+                setData(json.Search);
+                setFound(true);
+            }
+            else {
+                setFound(false);
+            }
 
         } catch (error) {
             console.log(error);
@@ -19,21 +27,25 @@ export function FetchMovies(props) {
         <Movie item={i} />
     ));
 
-    return (
-        <>
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>TITLE</th>
-                        <th>YEAR</th>
-                        <th>TYPE</th>
-                        <th>POSTER</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {res}
-                </tbody>
-            </table>
-        </>
-    );
+    let result = !found ? "Nothing found..." :
+        (
+            <>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>TITLE</th>
+                            <th>YEAR</th>
+                            <th>TYPE</th>
+                            <th>POSTER</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {res}
+                    </tbody>
+                </table>
+            </>
+        );
+
+
+    return result;
 }
