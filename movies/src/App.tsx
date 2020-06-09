9d21718796b10081fc814c3444c490ee6a61e1a5
kika <anamonteiro430@gmail.com>
2020-06-09 13:51:56 +0100

    setup the project, libraries and components folder

---
+++ movies/src/App.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
