8ca1051d0acf6392a3a3f93d28e8635c47e74482
Pedro Lisboa <pedrobslisboa@gmail.com>
2020-06-17 18:56:09 -0300

    [feature] Config precommit and eslint

---
+++ my-code/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'flowtype'],
+  rules: {}
+};
