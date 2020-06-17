c24cd2272791c0722718459b8c8c58b0c1e553d0
Pedro Lisboa <pedrobslisboa@gmail.com>
2020-06-17 18:44:09 -0300

    [bugfix] Next babel

--- my-code/next.config.js
+++ my-code/next.config.js
@@ -1,19 +1,16 @@
 require('dotenv').config();
 
 const path = require('path');
-const Dotenv = require('dotenv-webpack');
 const withCss = require('@zeit/next-css');
 
 const { NODE_ENV } = process.env;
 const prod = NODE_ENV === 'production';
 
-
 const nextConfig = {
   dir: './src',
-  generateBuildId: () => Promise.resolve(version),
 };
 
-module.exports = withImages(
+module.exports =
   withCss({
     cssModules: true,
     cssLoaderOptions: {
@@ -21,5 +18,4 @@ module.exports = withImages(
       localIdentName: prod ? 'significa-challenge-[hash:base64:5]' : '[name]__[local]--[hash:base64:5]'
     },
     ...nextConfig
-  })
-);
+  });
