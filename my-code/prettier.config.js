64211ad09c9a3feb85da61faf32386f6c988eac6
Pedro Lisboa <pedrobslisboa@gmail.com>
2020-06-17 16:45:53 -0300

    [feature] Add prettier to project and config it

---
+++ my-code/prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  endOfLine: 'lf'
+}
