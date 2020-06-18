ca62f9c7e686a34d1779daf13bf924111cd766d2
Pedro Lisboa <pedrobslisboa@gmail.com>
2020-06-18 19:39:39 -0300

    [tech debt] Remove marging and add className prop to SearchBar

--- my-code/src/components/shared/SearchBar/SearchBar.js
+++ my-code/src/components/shared/SearchBar/SearchBar.js
@@ -8,14 +8,15 @@ type Props = {
   onChange: () => void,
   value: string,
   disabled?: boolean,
-  placeholder?: string
+  placeholder?: string,
+  className?: string
 };
 
 export default function SearchBar(props: Props) {
-  const { onChange, value, disabled = false, placeholder } = props;
+  const { onChange, value, disabled = false, placeholder, className } = props;
 
   return (
-    <div className={classNames(styles.Wrapper, { [styles.disabled]: disabled })}>
+    <div className={classNames(styles.Wrapper, { [styles.disabled]: disabled }, className)}>
       <img src={magnifierIcon} className={styles.Magnifier} />
       <input disabled={disabled} value={value} onChange={onChange} placeholder={placeholder} className={styles.Input} />
     </div>
