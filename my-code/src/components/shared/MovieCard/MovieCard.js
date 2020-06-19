482798d41704320fb35e5ac11d1d5a46836215e7
Pedro Lisboa <pedrobslisboa@gmail.com>
2020-06-19 12:06:09 -0300

    [feature] Add fallbakc img to MovieCard

--- my-code/src/components/shared/MovieCard/MovieCard.js
+++ my-code/src/components/shared/MovieCard/MovieCard.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import classNames from 'classnames';
 import heartIconFull from '../../../icons/icon-heart-full.svg';
 import heartIconWhite from '../../../icons/icon-heart-white.svg';
 import styles from './MovieCard.css';
 import { useMoviesState } from '../../context/MoviesContext';
+import imgFallback from '../../../icons/logo-rotten-tomatoes.svg';
 
 type Props = {
   liked: boolean,
@@ -12,19 +13,19 @@ type Props = {
   id: string,
   poster: string,
   year: string,
-  className: string
+  className?: string
 };
 
 export default function MovieCard(props: Props) {
   const { title, year, poster, id, className } = props;
+  const [image, setImage] = useState(poster);
   const { state, dispatch } = useMoviesState();
   const isLiked = state.includes(id);
 
   const handleLike = () => {
     const isLiked = state.includes(id);
-    console.log(id);
     const actionType = isLiked ? 'UNLIKE' : 'LIKE';
-    console.log(state);
+
     dispatch({
       type: actionType,
       payload: {
@@ -33,11 +34,15 @@ export default function MovieCard(props: Props) {
     });
   };
 
+  const handleImageError = () => {
+    setImage(imgFallback);
+  };
+
   return (
     <div className={classNames(styles.Wrapper, className)}>
-      <img alt={`${title} Poster`} className={styles.Poster} src={poster} />
+      <img onError={handleImageError} alt={`${title} Poster`} className={styles.Poster} src={image} />
       <div className={styles.Info}>
-        <Link href="/movie/[id]" as={`/movie/${id}`}>
+        <Link shallow href="/movie/[id]" as={`/movie/${id}`}>
           <a className={styles.Content}>
             <h2 className={styles.Title}>{title}</h2>
             <span>{year}</span>
