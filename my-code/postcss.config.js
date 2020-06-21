272af1d40f3950a4ce0bc520c6a602c15ddbf69e
Pedro Lisboa <pedrobslisboa@gmail.com>
2020-06-21 12:39:56 -0300

    [feature] custom media for postcss

--- my-code/postcss.config.js
+++ my-code/postcss.config.js
@@ -3,7 +3,9 @@ module.exports = ({ env }) => ({
     cssnano: env === 'prod' ? {} : false,
     'postcss-import': {},
     'postcss-nesting': {},
+    'postcss-custom-media': {},
     'postcss-preset-env': {
+      stage: 1,
       preserve: false
     }
   }
