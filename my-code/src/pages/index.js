e8c5febbc06594f862731830bb6654fde8c41297
Pedro Lisboa <pedrobslisboa@gmail.com>
2020-06-21 16:17:47 -0300

    [tech debt] improve SEO at Page components

--- my-code/src/pages/index.js
+++ my-code/src/pages/index.js
@@ -1,8 +1,38 @@
+// @flow
 import React from 'react';
 import Search from '../components/pages/Search';
+import Head from 'next/head';
 
-const SearchPage = () => {
-  return <Search />;
+type Props = {
+  baseUrl: string
+};
+
+const SearchPage = (props: Props) => {
+  const { baseUrl } = props;
+
+  return (
+    <>
+      <Head>
+        <title>What&apos;s in</title>
+        <meta name="description" content="Search some movie to what" />
+        <meta name="robots" content="index,follow" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="canonical" href={`${baseUrl}/`} />
+        <meta property="og:title" content="What's in" />
+        <meta property="og:description" content="Search some movie to what" />
+        <meta property="og:url" content={`${baseUrl}/`} />
+        <meta property="og:locale" content="en" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <Search />
+    </>
+  );
+};
+
+export const getServerSideProps = () => {
+  const { SITE_API_BASE_URL } = process?.env;
+
+  return { props: { baseUrl: SITE_API_BASE_URL } };
 };
 
 export default SearchPage;
--- my-code/src/pages/movie/[id].js
+++ my-code/src/pages/movie/[id].js
@@ -1,18 +1,65 @@
+// @flow
 import React from 'react';
-import Movie from '../../components/pages/Movie';
+import MoviePage from '../../components/pages/Movie';
 import getMovie from '../../services/getMovie';
+import Head from 'next/head';
 
-export default function index(props) {
-  return <Movie {...props} />;
+export type MoviePageProps = {
+  movie: {
+    title: string,
+    runtime: string,
+    year: string,
+    rated: string,
+    poster: string,
+    ratings: Array<?{
+      Value: string,
+      Source: 'Internet Movie Database' | 'Rotten Tomatoes'
+    }>,
+    plot: string,
+    actors: string,
+    genre: string,
+    director: string,
+    id: string
+  },
+  error: boolean
+};
+
+type Props = {
+  movie: $PropertyType<MoviePageProps, 'movie'>,
+  error: $PropertyType<MoviePageProps, 'error'>,
+  baseUrl: string
+};
+
+export default function Movie(props: Props) {
+  const { baseUrl, movie, error } = props;
+  return (
+    <>
+      <Head>
+        <title>{movie.title} | What&apos;s in</title>
+        <meta name="description" content={movie.plot} />
+        <meta name="robots" content="index,follow" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="canonical" href={`${baseUrl}/`} />
+        <meta property="og:title" content={`${movie.title} | What's in`} />
+        <meta property="og:description" content={movie.plot} />
+        <meta property="og:url" content={`${baseUrl}/movie/${movie.id}`} />
+        <meta property="og:locale" content="en" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <MoviePage movie={movie} error={error} />
+    </>
+  );
 }
 
-export async function getServerSideProps({ params }) {
+export async function getServerSideProps({ params }: { params: { id: string } }) {
   const { id } = params;
   const { ok, movie } = await getMovie(id);
-  let props = {};
+  const { SITE_API_BASE_URL } = process?.env;
+  let props = { baseUrl: SITE_API_BASE_URL };
 
   if (ok) {
     props = {
+      ...props,
       movie,
       ok
     };
@@ -21,6 +68,6 @@ export async function getServerSideProps({ params }) {
   }
 
   return {
-    props: { ok: false, error: true }
+    props: { ...props, ok: false, error: true }
   };
 }
