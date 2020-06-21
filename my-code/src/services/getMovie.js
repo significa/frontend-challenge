f70c6ff65d8fbb31e0da311a6b395dfa88cb351b
Pedro Lisboa <pedrobslisboa@gmail.com>
2020-06-21 14:05:50 -0300

    [feature] Create getMovies service

---
+++ my-code/src/services/getMovie.js
@@ -0,0 +1,40 @@
+// @flow
+
+const url = 'http://localhost:3000/api/movie';
+
+type ReturnTypes = {
+  ok: boolean,
+  movie?: {
+    title: string,
+    runtime: string,
+    year: string,
+    rated: string,
+    poster: string,
+    ratings: Array<string>,
+    plot: string,
+    actors: string,
+    genre: string,
+    director: string
+  },
+  error?: any
+};
+
+export default async function getMovies(id: string): Promise<ReturnTypes> {
+  try {
+    const data = await fetch(`${url}?id=${id}`);
+
+    if (data.ok) {
+      const response = await data.json();
+
+      return {
+        movie: response,
+        ok: true
+      };
+    }
+
+    const response = data;
+    return response;
+  } catch (error) {
+    return { error, ok: false };
+  }
+}
