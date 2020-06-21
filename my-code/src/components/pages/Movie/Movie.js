950a9356fbaa04229e61b837e4e4a673150798d4
Pedro Lisboa <pedrobslisboa@gmail.com>
2020-06-21 14:05:13 -0300

    [feature] Create Movie page

---
+++ my-code/src/components/pages/Movie/Movie.js
@@ -0,0 +1,177 @@
+// @flow
+import React from 'react';
+import Layout from '../../shared/Layout';
+import Router from 'next/router';
+import classNames from 'classnames';
+import styles from './Movie.css';
+import arrowIconGrey from '../../../icons/icon-arrow-grey.svg';
+import arrowIconWhite from '../../../icons/icon-arrow-white.svg';
+import imdblogo from '../../../icons/logo-imdb.svg';
+import rottenTomatoesLogo from '../../../icons/logo-rotten-tomatoes.svg';
+import heartGreyIcon from '../../../icons/icon-heart-grey.svg';
+import heartFullIcon from '../../../icons/icon-heart-full.svg';
+import useLike from '../../hooks/useLike';
+
+type Props = {
+  movie: {
+    title: string,
+    runtime: string,
+    year: string,
+    rated: string,
+    poster: string,
+    ratings: Array<?{
+      Value: string,
+      Source: 'Internet Movie Database' | 'Rotten Tomatoes'
+    }>,
+    plot: string,
+    actors: string,
+    genre: string,
+    director: string,
+    id: string
+  },
+  error: boolean
+};
+
+export default function Movie(props: Props) {
+  const { movie, error } = props;
+  const { title, runtime, year, id, rated, poster, ratings, plot, actors, genre, director } = movie;
+
+  if (error) {
+    return (
+      <Layout>
+        <div className={styles.ErrorPage}>
+          <img className={styles.ErrorImg} src={rottenTomatoesLogo} />
+          <span>Something went wrong :/</span>
+        </div>
+      </Layout>
+    );
+  }
+
+  if (Object.keys(movie).length === 0 && movie?.constructor === Object) {
+    return (
+      <Layout>
+        <div className={styles.ErrorPage}>
+          <img className={styles.ErrorImg} src={rottenTomatoesLogo} />
+          <span>Movie not found :/</span>
+        </div>
+      </Layout>
+    );
+  }
+
+  const actorsList = actors.split(', ');
+  const genreList = genre.split(', ');
+  const directorList = director.split(', ');
+
+  const { liked, toggle } = useLike(id);
+
+  const handleLike = () => {
+    toggle();
+  };
+
+  const handleBackHistory = () => Router.back();
+
+  return (
+    <Layout>
+      <div>
+        <button title="Back" className={styles.BackButton} onClick={handleBackHistory}>
+          <img
+            onMouseOver={(e) => (e.currentTarget.src = arrowIconWhite)}
+            onMouseLeave={(e) => (e.currentTarget.src = arrowIconGrey)}
+            src={arrowIconGrey}
+            alt="Back icon"
+            className={styles.ArrowIcon}
+          />
+        </button>
+      </div>
+      <div className={styles.Movie}>
+        <div className={styles.MovieContent}>
+          <div className={styles.MovieInfo}>
+            <span>
+              {runtime} &#183; {year} &#183;
+            </span>
+            <span className={styles.Rated}>{rated}</span>
+          </div>
+          <h1 className={styles.Title}>{title}</h1>
+          <section className={styles.Badges}>
+            {ratings.length > 0 &&
+              ratings.map((rating) => {
+                if (rating?.Source === 'Internet Movie Database') {
+                  return (
+                    <div key={rating.Source} className={styles.Badge}>
+                      <div className={styles.Imdblogo}>
+                        <img alt="IMDB logo" src={imdblogo} />
+                      </div>
+                      <span className={styles.Ratting}>{rating?.Value}</span>
+                    </div>
+                  );
+                }
+
+                if (rating?.Source === 'Rotten Tomatoes') {
+                  return (
+                    <div key={rating?.Source} className={styles.Badge}>
+                      <div className={styles.Rottenlogo}>
+                        <img atl="Rotten Tomatoes logo" src={rottenTomatoesLogo} />
+                      </div>
+                      <span className={styles.Ratting}>{rating?.Value}</span>
+                    </div>
+                  );
+                }
+              })}
+            <button
+              title={liked ? 'Added' : 'Add to favourites'}
+              onClick={handleLike}
+              className={classNames(styles.LikeButton, { [styles.liked]: liked })}
+            >
+              <img alt="heart icon" className={styles.HeartIcon} src={liked ? heartFullIcon : heartGreyIcon} />
+              {liked ? 'Added' : 'Add to favourites'}
+            </button>
+          </section>
+          <section className={styles.MovieDetails}>
+            <div>
+              <h2 className={styles.PlotHeading}>Plot</h2>
+              <p className={styles.PlotText}>{plot}</p>
+            </div>
+            <div className={styles.Lists}>
+              <div>
+                <ul className={styles.DetailsList}>
+                  <h2 className={styles.DetailsHeading}>Cast</h2>
+                  {actorsList.map((cast) => (
+                    <li key={cast}>{cast.trim()}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <ul className={styles.DetailsList}>
+                  <h2 className={styles.DetailsHeading}>Genre</h2>
+                  {genreList.map((cast) => (
+                    <li key={cast}>{cast.trim()}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <ul className={styles.DetailsList}>
+                  <h2 className={styles.DetailsHeading}>Director</h2>
+                  {directorList.map((cast) => (
+                    <li key={cast}>{cast.trim()}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </section>
+        </div>
+        {/** Hack to deal with onError for ssr page */}
+        <div
+          className={styles.Poster}
+          dangerouslySetInnerHTML={{
+            __html: `
+          <img alt="${title} Poster" src="${poster}"
+          class="${styles.PosterImg}"
+          onerror="this.parentElement.remove()"
+          />
+            `
+          }}
+        />
+      </div>
+    </Layout>
+  );
+}
---
+++ my-code/src/components/pages/Movie/__tests__/Movie.test.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Movie from '../Movie';
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+}
+
+global.localStorage = new LocalStorageMock();
+
+const resultsResolveMock = {
+  ok: true,
+  movie: {
+    title: 'What Women Want',
+    year: '2000',
+    rated: 'PG-13',
+    released: '15 Dec 2000',
+    runtime: '127 min',
+    genre: 'Comedy, Fantasy, Romance',
+    director: 'Nancy Meyers',
+    writer:
+      'Josh Goldsmith (story), Cathy Yuspa (story), Diane Drake (story), Josh Goldsmith (screenplay), Cathy Yuspa (screenplay)',
+    actors: 'Mel Gibson, Helen Hunt, Marisa Tomei, Alan Alda',
+    plot: 'After an accident, a chauvinistic executive gains the ability to hear what women are really thinking.',
+    poster:
+      'https://m.media-amazon.com/images/M/MV5BZjUyZWE5YmMtNDA2ZC00NzFlLTg0MzktOTgzYjA2ZWE3NmIwXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+    ratings: [
+      {
+        Source: 'Internet Movie Database',
+        Value: '6.4/10'
+      },
+      {
+        Source: 'Rotten Tomatoes',
+        Value: '54%'
+      },
+      {
+        Source: 'Metacritic',
+        Value: '47/100'
+      }
+    ]
+  }
+};
+
+describe('<MovieBar />', () => {
+  it('should render with title', async () => {
+    render(<Movie {...resultsResolveMock} />);
+
+    expect(screen.getByRole('heading', { name: /What Women Want/ })).toBeInTheDocument();
+  });
+  it('should render with plot', async () => {
+    render(<Movie {...resultsResolveMock} />);
+
+    expect(screen.getByRole('heading', { name: /Plot/ })).toBeInTheDocument();
+  });
+
+  it('should render with cast list', async () => {
+    render(<Movie {...resultsResolveMock} />);
+
+    const genre = resultsResolveMock.movie.genre.split(', ');
+
+    expect(screen.getByRole('heading', { name: /Genre/ })).toBeInTheDocument();
+    genre.forEach((item) => {
+      expect(screen.getByText(item)).toBeInTheDocument();
+    });
+  });
+
+  it('should render with director list', async () => {
+    render(<Movie {...resultsResolveMock} />);
+
+    const actors = resultsResolveMock.movie.actors.split(', ');
+
+    expect(screen.getByRole('heading', { name: /Cast/ })).toBeInTheDocument();
+    actors.forEach((item) => {
+      expect(screen.getByText(item)).toBeInTheDocument();
+    });
+  });
+
+  it('should render with director list', async () => {
+    render(<Movie {...resultsResolveMock} />);
+
+    const director = resultsResolveMock.movie.actors.split(', ');
+
+    expect(screen.getByRole('heading', { name: /Director/ })).toBeInTheDocument();
+    director.forEach((item) => {
+      expect(screen.getByText(item)).toBeInTheDocument();
+    });
+  });
+
+  it('should render Ratings', async () => {
+    render(<Movie {...resultsResolveMock} />);
+
+    expect(screen.getByText('6.4/10')).toBeInTheDocument();
+    expect(screen.getByText('54%')).toBeInTheDocument();
+  });
+
+  it('should not render metacritic rating', async () => {
+    render(<Movie {...resultsResolveMock} />);
+
+    expect(screen.queryByText('47/100')).not.toBeInTheDocument();
+  });
+});
