4491bdebcb1422aced6a84055ded7b539abc75d0
Pedro Lisboa <pedrobslisboa@gmail.com>
2020-06-21 12:19:11 -0300

    [bugfix] remove Provider from _app

--- my-code/src/pages/_app.js
+++ my-code/src/pages/_app.js
@@ -1,6 +1,5 @@
 // @flow
 import * as React from 'react';
-import { MoviesProvider } from '../components/context/MoviesContext';
 import './_app.css';
 
 type Props = {
@@ -13,23 +12,7 @@ type Props = {
 const CustomApp = (props: Props) => {
   const { Component, pageProps } = props;
 
-  return (
-    <MoviesProvider>
-      <Component pageProps={pageProps} />
-    </MoviesProvider>
-  );
-};
-
-CustomApp.getInitialProps = async ({ Component, ctx }) => {
-  let pageProps = {};
-
-  if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx);
-  }
-
-  return {
-    pageProps
-  };
+  return <Component {...pageProps} />;
 };
 
 export default CustomApp;
