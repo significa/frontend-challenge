0506ff3e7434a619e3fe0477a5736f8cb07f2636
cleysonlb <cleysonwebdev@gmail.com>
2020-12-04 18:04:26 -0300

    [enhancement] add favicons and enhancement SEO

--- my-code/src/pages/index.js
+++ my-code/src/pages/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import Head from 'next/head';
+import getConfig from 'next/config';
 import Search from '../components/pages/Search';
 
-const SearchPage = props => <Search {...props} />;
+const SearchPage = props => {
+  const { publicRuntimeConfig } = getConfig();
+  const { siteApiUrl } = publicRuntimeConfig;
+  return (
+    <>
+      <Head>
+        <title>Whats in</title>
+        <meta name="robots" content="index,follow" />
+        <meta name="description" content="Search for a movie" />
+        <link rel="canonical" href={siteApiUrl} />
+      </Head>
+      <Search {...props} />
+    </>
+  );
+};
 
 export default SearchPage;
--- my-code/src/pages/movie/[id].js
+++ my-code/src/pages/movie/[id].js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Head from 'next/head';
+import getConfig from 'next/config';
+import PropTypes from 'prop-types';
 import getMovie from '../../services/getMovie';
 import Movie from '../../components/pages/Movie';
 
-const MoviePage = props => <Movie {...props} />;
+const MoviePage = props => {
+  const { publicRuntimeConfig } = getConfig();
+  const { siteApiUrl } = publicRuntimeConfig;
+  const { movie } = props;
+  return (
+    <>
+      <Head>
+        <title>{movie?.Title || "What's in"}</title>
+        <meta name="robots" content="index,follow" />
+        <meta name="description" content={movie?.Plot || ''} />
+        <link rel="canonical" href={`${siteApiUrl}/`} />
+      </Head>
+      <Movie {...props} />
+    </>
+  );
+};
 
 export async function getServerSideProps({ query }) {
   const { id } = query;
@@ -12,5 +30,27 @@ export async function getServerSideProps({ query }) {
     props: { movie: data }
   };
 }
+
+MoviePage.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string,
+    Year: PropTypes.string,
+    Actors: PropTypes.string,
+    Rated: PropTypes.string,
+    Runtime: PropTypes.string,
+    Genre: PropTypes.string,
+    Director: PropTypes.string,
+    Plot: PropTypes.string,
+    Ratings: PropTypes.arrayOf(
+      PropTypes.shape({
+        Source: PropTypes.string,
+        Value: PropTypes.string
+      })
+    ),
+    Poster: PropTypes.string,
+    imdbID: PropTypes.string,
+    Response: PropTypes.string
+  })
+};
 
 export default MoviePage;
