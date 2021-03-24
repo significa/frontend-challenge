ff6ad907484e27fa05928fe46be7ebcb81b23ee9
Vitor Moreno <vh.morenodasilva@gmail.com>
2021-03-24 19:04:14 -0300

    Add useLike hook

---
+++ my-code/frontend/src/hooks/useLike.ts
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default function useLike(imdbID) {
+  const [liked, setLiked] = React.useState<boolean>(() => {
+    const likedMovies = localStorage.getItem("@App/likedMovies");
+    let parsed;
+    try {
+      parsed = JSON.parse(likedMovies);
+    } catch {
+      parsed = {};
+    }
+    // Set liked initial state
+    if (parsed[imdbID]) {
+      return true;
+    }
+    return false;
+  });
+
+  React.useEffect(() => {
+    const likedMovies = localStorage.getItem("@App/likedMovies");
+    let parsed;
+
+    // Avoid JSON.Parse error if the stringified object is malformed for some reason
+    try {
+      parsed = JSON.parse(likedMovies);
+    } catch {
+      console.log("Failed to parse JSON...");
+      parsed = {};
+    }
+
+    const newLikedMovies = {
+      ...parsed,
+      [imdbID]: liked,
+    };
+
+    // Save likes to local storage
+    localStorage.setItem("@App/likedMovies", JSON.stringify(newLikedMovies));
+  }, [liked]);
+
+  const handleLike = (e: React.MouseEvent<HTMLButtonElement>) => {
+    // We need to stop the event propagation from bubbling up here
+    // because we don't want the like button to trigger the route change.
+    e.stopPropagation();
+    setLiked(!liked);
+  };
+
+  return { liked, handleLike };
+}
