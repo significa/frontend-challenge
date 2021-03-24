9cd7046d33107dd8244fbf9682e3b654e5befb5c
Vitor Moreno <vh.morenodasilva@gmail.com>
2021-03-24 11:37:47 -0300

    🏷️ Update theme colors

--- my-code/frontend/src/themes/ITheme.ts
+++ my-code/frontend/src/themes/ITheme.ts
@@ -1,7 +1,11 @@
 export default interface {
-  background: string,
-  fontColor: string,
-  fontColorVariant: string,
-  border: string,
-  borderVariant: string,
+  yellow: string;
+  red: string;
+  green: string;
+  
+  background: string;
+  fontColor: string;
+  fontColorVariant: string;
+  border: string;
+  borderVariant: string;
 };
--- my-code/frontend/src/themes/dark.ts
+++ my-code/frontend/src/themes/dark.ts
@@ -1,6 +1,10 @@
 import ITheme from "./ITheme";
 
 const theme: ITheme = {
+  yellow: "#FF9F1C",
+  red: "#FF4040",
+  green: "#2EC4B6",
+
   background: "#0A1014",
   fontColor: "#FFFFFF",
   fontColorVariant: "#7A8C99",
