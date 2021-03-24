7928bdd17f00b9be2f2b6b83e52b9a9b3fa7ab52
Vitor Moreno <vh.morenodasilva@gmail.com>
2021-03-24 19:07:10 -0300

    ♻️ Update moviecard to use useLike hook

--- my-code/frontend/src/components/MovieCard/index.tsx
+++ my-code/frontend/src/components/MovieCard/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Overlay } from "./styles";
 import { useHistory } from "react-router";
+import useLike from "../../hooks/useLike";
 interface IProps {
   Poster: string;
   Title: string;
@@ -11,49 +12,7 @@ interface IProps {
 
 const MovieCard: React.FC<IProps> = ({ Poster, Title, Year, imdbID }) => {
   const history = useHistory();
-  // Query localstorage before defining `liked` initial state
-  const [liked, setLiked] = React.useState<boolean>(() => {
-    const likedMovies = localStorage.getItem("@App/likedMovies");
-    let parsed;
-    try {
-      parsed = JSON.parse(likedMovies);
-    } catch {
-      parsed = {};
-    }
-    // Set liked initial state
-    if (parsed[imdbID]) {
-      return true;
-    }
-    return false;
-  });
-
-  React.useEffect(() => {
-    const likedMovies = localStorage.getItem("@App/likedMovies");
-    let parsed;
-
-    // Avoid JSON.Parse error if the stringified object is malformed for some reason
-    try {
-      parsed = JSON.parse(likedMovies);
-    } catch {
-      console.log("Failed to parse JSON...");
-      parsed = {};
-    }
-
-    const newLikedMovies = {
-      ...parsed,
-      [imdbID]: liked,
-    };
-
-    // Save likes to local storage
-    localStorage.setItem("@App/likedMovies", JSON.stringify(newLikedMovies));
-  }, [liked]);
-
-  const handleLike = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // We need to stop the event propagation from bubbling up here
-    // because we don't want the like button to trigger the route change.
-    e.stopPropagation();
-    setLiked(!liked);
-  };
+  const { liked, handleLike } = useLike(imdbID);
 
   const handleRoute = () => {
     history.push(`/m/${imdbID}`);
