25da4845681f30eca8e06a33b39d27a5fe6a75c1
Vitor Moreno <vh.morenodasilva@gmail.com>
2021-03-26 14:23:23 -0300

    ✨ Enable cors

--- my-code/backend/src/server.ts
+++ my-code/backend/src/server.ts
@@ -1,8 +1,10 @@
 require("dotenv/config");
 import express from "express";
+import cors from "cors";
 import fetch from "node-fetch";
 
 const app = express();
+app.use(cors());
 
 app.get("/search", async (req, res) => {
   try {
