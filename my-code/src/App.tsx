7170b484420ccea4596b3744af102ba487b0c34b
Kipkemoi Emmanuel <kipkemoi@Kipkemois-MacBook-Air.local>
2022-02-04 17:56:10 +0300

    added routing to app

--- my-code/src/App.tsx
+++ my-code/src/App.tsx
@@ -1,9 +1,15 @@
+import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Detail from './components/detail/detail';
+import Home from './components/home/home';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Route path='/movie/:id'><Detail /></Route>
+        <Route path='/'><Home /></Route>
+      </BrowserRouter>
     </div>
   );
 }
