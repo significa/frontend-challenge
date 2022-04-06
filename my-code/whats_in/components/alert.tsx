ee3c02896bf9dc7fa9f1fab313993ad700de0583
jeffubayi <jeffubayi@gmail.com>
2022-04-06 10:11:17 +0300

    add an error alert component

---
+++ my-code/whats_in/components/alert.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Alert from "@mui/material/Alert";
+
+const Alerts = () => {
+  return (
+    <div>
+      <Alert severity="error">Movie Not Found</Alert>
+    </div>
+  );
+};
+export default Alerts;
