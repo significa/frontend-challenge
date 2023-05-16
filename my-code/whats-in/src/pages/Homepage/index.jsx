14f3d1972c8345c006669875234fd7a9c024a4af
Fernanda Freitas <idea.fernanda@gmail.com>
2023-05-16 17:27:48 +0100

    Created Empty state component

--- my-code/whats-in/src/pages/Homepage/index.jsx
+++ my-code/whats-in/src/pages/Homepage/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logotype from '../../images/logo.svg'
 import iconSearch from '../../images/icon-magnifier-grey.svg'
+import empty from '../../images/illustration-empty-state@2x.png'
 
 export default function Homepage() {
     return (
@@ -11,6 +12,11 @@ export default function Homepage() {
             <div className='search'>
                 <input className='search__input' type="text" placeholder='Search movies...'/>
                 <img className='search__icon' src={iconSearch} alt="Search icon" />
+            </div>
+            <div className='empty'>
+                <img className='empty__image' src={empty} alt="Empty search" />
+                <h2>Don’t know what to search?</h2>
+                <p className='empty__subheader--lightgrey'>Here’s an offer you can’t refuse</p>
             </div>
         </div>
     )
