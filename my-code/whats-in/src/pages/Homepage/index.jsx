c3a9279f3a3c3331d825aae75915945a609da0ea
Fernanda Freitas <idea.fernanda@gmail.com>
2023-05-16 17:14:47 +0100

    Header and Search bar style

--- my-code/whats-in/src/pages/Homepage/index.jsx
+++ my-code/whats-in/src/pages/Homepage/index.jsx
@@ -1,7 +1,17 @@
+import React from 'react';
+import logotype from '../../images/logo.svg'
+import iconSearch from '../../images/icon-magnifier-grey.svg'
+
 export default function Homepage() {
     return (
         <div className="grid">
-            <div className="item"></div>
+            <div className="header">
+                <img src={logotype} alt="logotype" />
+            </div>
+            <div className='search'>
+                <input className='search__input' type="text" placeholder='Search movies...'/>
+                <img className='search__icon' src={iconSearch} alt="Search icon" />
+            </div>
         </div>
     )
 }
