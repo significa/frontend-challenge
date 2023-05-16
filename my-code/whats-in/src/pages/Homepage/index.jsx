51c4713358f76b386a87a25c552613c03e190144
Fernanda Freitas <idea.fernanda@gmail.com>
2023-05-16 18:06:06 +0100

    Defined hover on card and Like onClick event

--- my-code/whats-in/src/pages/Homepage/index.jsx
+++ my-code/whats-in/src/pages/Homepage/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logotype from '../../images/logo.svg'
 import iconSearch from '../../images/icon-magnifier-grey.svg'
-import empty from '../../images/illustration-empty-state@2x.png'
+import emptyImage from '../../images/illustration-empty-state@2x.png'
 import movieImage from '../../images/removeaftertest.png'
+import iconLike from '../../images/icon-heart-grey.svg'
+import iconLiked from '../../images/icon-heart-full.svg'
 
 export default function Homepage() {
     const isVisible = false;
+    const [liked, setLiked] = useState(false)
+
+    const handleClick = () => {
+        setLiked(!liked)
+    }
 
     return (
         <div className="grid">
@@ -18,7 +25,7 @@ export default function Homepage() {
             </div>
             {isVisible && (
                 <div className='empty'>
-                    <img className='empty__image' src={empty} alt="Empty search" />
+                    <img className='empty__image' src={emptyImage} alt="Empty search" />
                     <h2>Don’t know what to search?</h2>
                     <p className='empty__subheader--lightgrey'>Here’s an offer you can’t refuse</p>
                 </div>
@@ -26,7 +33,13 @@ export default function Homepage() {
             <div className='movie-card'>
                 <img className='movie-card__image' src={movieImage} alt="Movie card" />
                 <div className='movie-card__overlay'>
-                    <p>testing</p>
+                    <button className='movie-card__like' onClick={handleClick}>
+                        <img src={liked ? iconLiked : iconLike} alt="Like button" />
+                    </button>
+                    <div className='movie-card__description'>
+                        <h4 className='movie-card__description__title'>Movie name</h4>
+                        <p className='movie-card__description__year'>2018</p>
+                    </div>
                 </div>
             </div>
         </div>
