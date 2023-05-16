17c0bebe2ccb82bfa3890a1724243b1b4fe26047
Fernanda Freitas <idea.fernanda@gmail.com>
2023-05-16 17:45:32 +0100

    Defined movies card deck and hover

--- my-code/whats-in/src/pages/Homepage/index.jsx
+++ my-code/whats-in/src/pages/Homepage/index.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import logotype from '../../images/logo.svg'
 import iconSearch from '../../images/icon-magnifier-grey.svg'
 import empty from '../../images/illustration-empty-state@2x.png'
+import movieImage from '../../images/removeaftertest.png'
 
 export default function Homepage() {
+    const isVisible = false;
+
     return (
         <div className="grid">
             <div className="header">
@@ -13,10 +16,18 @@ export default function Homepage() {
                 <input className='search__input' type="text" placeholder='Search movies...'/>
                 <img className='search__icon' src={iconSearch} alt="Search icon" />
             </div>
-            <div className='empty'>
-                <img className='empty__image' src={empty} alt="Empty search" />
-                <h2>Don’t know what to search?</h2>
-                <p className='empty__subheader--lightgrey'>Here’s an offer you can’t refuse</p>
+            {isVisible && (
+                <div className='empty'>
+                    <img className='empty__image' src={empty} alt="Empty search" />
+                    <h2>Don’t know what to search?</h2>
+                    <p className='empty__subheader--lightgrey'>Here’s an offer you can’t refuse</p>
+                </div>
+            )}
+            <div className='movie-card'>
+                <img className='movie-card__image' src={movieImage} alt="Movie card" />
+                <div className='movie-card__overlay'>
+                    <p>testing</p>
+                </div>
             </div>
         </div>
     )
