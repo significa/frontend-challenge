05bb4716b5e5f2ed4108475f2de28c11b320780c
Fernanda Freitas <idea.fernanda@gmail.com>
2023-05-16 15:23:43 +0100

    Added a container system and a grid system

--- my-code/whats-in/src/App.jsx
+++ my-code/whats-in/src/App.jsx
@@ -4,19 +4,16 @@ import Moviepage from './pages/Movie'
 
 function App() {
   return (
-    <div className='container-grid'>
-      <h1>teste</h1>
-      <h2>teste</h2>
-      <h3>teste</h3>
-      <h4>teste</h4>
-      <h5>teste</h5>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, quas. Quod totam odit necessitatibus earum omnis beatae voluptates. Similique nam veritatis fuga architecto, deleniti alias officia in ratione temporibus molestiae sit facere delectus assumenda quas, neque dicta necessitatibus eveniet adipisci nulla distinctio quod magni. Dignissimos illum accusamus sequi repellat quasi.</p>
-      <Router>
-        <Routes>
-            <Route exact path="/" element={<Homepage/>}/>
-            <Route path="/movie" element={<Moviepage />}/>
-        </Routes>
-      </Router>
+    <div className='container'>
+      <div className='grid'>
+        <div className='item'></div>
+        <Router>
+          <Routes>
+              <Route exact path="/" element={<Homepage/>}/>
+              <Route path="/movie" element={<Moviepage />}/>
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
