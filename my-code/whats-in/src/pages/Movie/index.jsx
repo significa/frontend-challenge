3e721379becf14d2456a3f38508df25338c2b7b7
Fernanda Freitas <idea.fernanda@gmail.com>
2023-05-17 13:35:51 +0100

    Definition of left structure from movie page

--- my-code/whats-in/src/pages/Movie/index.jsx
+++ my-code/whats-in/src/pages/Movie/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import iconBack from '../../images/icon-arrow-grey.svg'
+import logoImdb from '../../images/logo-imdb.svg'
 
 export default function Moviepage() {
     const handleClick = () => {
@@ -13,11 +14,17 @@ export default function Moviepage() {
                     <img src={iconBack} alt="Back button link" />
                 </button>
                 <div className='movie-info__details'>
-                    <h3 className='movie-info__duration'>86 min</h3>
-                    <h3 className='movie-info__year'>2014</h3>
-                    <span className='movie-info__classification'>R</span>
+                    <h3 className='movie-info__details--duration'>86 min</h3>
+                    <h3 className='movie-info__details--year'>2014</h3>
+                    <span className='movie-info__details--label'>R</span>
                 </div>
-                <h1>What We Do in the Shadows</h1>
+                <h1 className='movie-info__title'>What We Do in the Shadows</h1>
+                <div className='movie-info__rating-bar'>
+                    <div className='movie-info__rating-bar--item'>
+                        <img className='movie-info__rating-bar--item__image' src={logoImdb} alt="Imdb logo" />
+                        <p className='movie-info__rating-bar--item__text'>7.6/10</p>
+                    </div>
+                </div>
             </div>
             <div className="movie-image">
                 <img src="" alt="" />
