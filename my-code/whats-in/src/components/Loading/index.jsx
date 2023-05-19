4cf95ac7eb9e914d604e96e8a07401fb12c386de
Fernanda Freitas <idea.fernanda@gmail.com>
2023-05-19 16:50:28 +0100

    Removed unnessessary scss files

---
+++ my-code/whats-in/src/components/Loading/index.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="loading">
+          <div className="film-reel"></div>
+        </div>
+    )
+}
--- my-code/whats-in/src/pages/Homepage/index.jsx
+++ my-code/whats-in/src/pages/Homepage/index.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import emptyImage from '../../images/illustration-empty-state@2x.png'
 import iconSearch from '../../images/icon-magnifier-grey.svg'
 import iconLike from '../../images/icon-heart-grey.svg'
 import iconLiked from '../../images/icon-heart-full.svg'
-
+import Loading from '../../components/Loading'
+import Empty from '../../components/Empty'
 
 export default function Homepage({ handleSearch, movies, handleMovieSelection, isLoading }) {
     const [liked, setLiked] = useState(false)
@@ -26,29 +26,27 @@ export default function Homepage({ handleSearch, movies, handleMovieSelection, i
                 <input onChange={handleChange} className='search__input' type="text" placeholder='Search movies...'/>
                 <img className='search__icon' src={iconSearch} alt="Search icon" />
             </div>
-            {isLoading && (
-                <h3>Loding</h3>
-            )}
-            {movies ? (movies.map((movie) => (
-                <div onClick={() => handleSelection(movie)} className='movie-card' key={movie.imdbID}>
-                    <img className='movie-card__image' src={movie.Poster} alt="Movie card" />
-                    <div className='movie-card__overlay'>
-                        <button className='button-icon movie-card__like' onClick={handleLike}>
-                            <img src={liked ? iconLiked : iconLike} alt="Like button" />
-                        </button>
-                        <div className='movie-card__description'>
-                            <h4 className='movie-card__description__title'>{movie.Title}</h4>
-                            <p className='movie-card__description__year'>{movie.Year}</p>
+            {isLoading ? (
+                <Loading />
+                ) : (
+                movies && (movies.map((movie) => (
+                    <div onClick={() => handleSelection(movie)} className='movie-card' key={movie.imdbID}>
+                        <img className='movie-card__image' src={movie.Poster} alt="Movie card" />
+                        <div className='movie-card__overlay'>
+                            <button className='button-icon movie-card__like' onClick={handleLike}>
+                                <img src={liked ? iconLiked : iconLike} alt="Like button" />
+                            </button>
+                            <div className='movie-card__description'>
+                                <h4 className='movie-card__description__title'>{movie.Title}</h4>
+                                <p className='movie-card__description__year'>{movie.Year}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    ))
                 ))
-            ): (
-            <div className='empty'>
-                <img className='empty__image' src={emptyImage} alt="Empty search" />
-                <h2>Don’t know what to search?</h2>
-                <p className='empty__subheader--lightgrey'>Here’s an offer you can’t refuse</p>
-            </div>
+            }
+            {!movies && (
+                <Empty />
             )}
         </div>
     )
